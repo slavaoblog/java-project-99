a33907bc1ed7e1eefeae8fa77b57603d29e0cacc
Viacheslav Oblog <slavaoblog@ya.ru>
2023-11-11 14:13:27 +0200

    added gradle project

---
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -0,0 +1,27 @@
+package hexlet.code.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AppApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testIndex() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/welcome"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+}
